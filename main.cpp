4175a2634c30fc64453a3eae8c5db25c462813c1
lmmckinley1 <lmmckinley1@o244-28.csuchico.edu>
2019-09-23 16:38:19 -0700

    Horoscope asking for name

--- main.cpp
+++ main.cpp
@@ -1,4 +1,4 @@
-//Authors:
+//Author: Leah McKinley
 #include<iostream>
 #include<string>
 
@@ -7,8 +7,17 @@ using namespace std;
 int main()
 {
   //declare variables
+  
+        string firstName = " ";
+        string lastName = " ";
 
   //get user input
+
+        cout<<"What is your first name? ";
+        cin>>firstName;
+        cout<<"What is your last name? ";
+        cin>>lastName;
+        cout<<"Welcome, "<<firstName.at(0)<<"."<<lastName.at(0)<<"."<<", her is your fortune... "<<endl;
 
   //tell fortune
 
