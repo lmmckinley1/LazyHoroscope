d9a2e87c719ddbde45ee5c5281303591d0a2a1be
lmmckinley1 <lmmckinley1@ecc-jaguar.csuchico.edu>
2019-09-25 20:52:16 -0700

    Lazy Horoscope Vowel

--- main.cpp
+++ main.cpp
@@ -10,6 +10,11 @@ int main()
   
         string firstName = " ";
         string lastName = " ";
+        char c;
+        int lowercaseVowel;
+        int uppercaseVowel;
+        lowercaseVowel = (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u');
+        uppercaseVowel = (c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U');
 
   //get user input
 
@@ -21,6 +26,23 @@ int main()
   //tell fortune
 
         cout<<"your lucky number is "<<firstName.length()<<endl;
+        
+        if (firstName.at(0) == 'a' || firstName.at(0) == 'e' || firstName.at(0) == 'i' || firstName.at(0) == 'o' || firstName.at(0) == 'u' || firstName.at(0) == 'A' || firstName.at(0) == 'E' || firstName.at(0) == 'I' || firstName.at(0) == 'O' || firstName.at(0) == 'U')
+
+        {   
+            cout<<"you should keep a low profile."<<endl;
+        }
+        else
+        {
+            cout<<"you are destined to be famous!"<<endl;
+        }
+      
+        if (lastName.at(lastName.length()-1) == 'a' || lastName.at(lastName.length()-1) =='e' || lastName.at(lastName.length()-1) == 'i' || lastName.at(lastName.length()-1) =='o' ||lastName.at(lastName.length()-1) == 'u' || lastName.at(lastName.length()-1) == 'A' || lastName.at(lastName.length()-1) == 'E' || lastName.at(lastName.length()-1) == 'I' || lastName.at(lastName.length()-1) == 'O' || lastName.at(lastName.length()-1) == 'U')
+       { 
+            cout<<"you have already met your true love."<<endl;
+        }
+
+        cout<<"have a good day!"<<endl;
 
   return 0;
 }
